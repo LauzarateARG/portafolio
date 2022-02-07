cec4f63bbe53f65d8bc8cf725fa9df0c374e7e2a
LauzarateARG <lautarozarate212001@gmail.com>
2022-02-06 23:09:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      "hola mundo"
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function navBar() {
+  return(
+      <div className='navBar'>
+          <h1>Hola mundo</h1>
+          <ul>
+            <ui>Home</ui>
+            <ui>About Me</ui>
+            <ui>Proyects</ui>
+            <ui>Contact Me</ui>
+          </ul>
+      </div>
+  );
+}
+
+export default navBar;
