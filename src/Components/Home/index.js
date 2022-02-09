4edf2ebc0db17f5239ce3d8ac7a7b7a88609ea1f
LauzarateARG <lautarozarate212001@gmail.com>
2022-02-09 15:12:22 -0300

    finish SlideShow

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import "./index.css"
+import React from "react";
+import "./index.css";
 function home() {
+
+  function seeProyects(){
+    alert("See projects")
+  }
+
+  function downloadCV(){
+    alert("Download CV")
+  }
+
   return (
-    <div className='home'>
-      Hola a todo el mundo
+    <div className="home">
+      <div className="cvContain">
+        <p className="name">Hi, I'm Lautaro Zarate</p>
+        <p className="job">Web Developer</p>
+        <p className="descrption">
+          This is my personal portfolio, here you can see the projects I worked
+          on and also my personal information
+        </p>
+        <div className="btnContain">
+          <button className="btn1" onClick={seeProyects} > See projects </button>
+          <button className="btn2" onClick={downloadCV} > Download CV </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/SlideShow/index.js
+++ src/Components/SlideShow/index.js
@@ -1,38 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.css";
 
 //import Assets
 import img1 from "../../assets/1.png";
 import img2 from "../../assets/2.png";
-import { ReactComponent as RightArrow } from "../../assets/iconmonstr-angel-right-thin.svg";
-import { ReactComponent as LeftArrow } from "../../assets/iconmonstr-angel-left-thin.svg";
-function slideShow() {
+
+export default function SlideShow() {
+  const slideContainer = useRef(null);
+  const intervalSlideShow = useRef(null);
+
+  function testeo() {
+    console.log(slideContainer.current.children[0]);
+  }
+
+  function next() {
+    if (slideContainer.current.children.length > 0) {
+      //Obtener el primer elemento del slide contain
+      const firstElement = slideContainer.current.children[0];
+
+      //Establecemos la transicion ára el slide contain
+      slideContainer.current.style.transition = "1500ms ease-out all";
+
+      const slideSize = slideContainer.current.children[0].offsetWidth;
+
+      //Movemos el slide contain
+      slideContainer.current.style.transform = `translateX(-${slideSize}px)`;
+
+      const transicion = () => {
+        //reiniciamos la posicion del slide contain
+        slideContainer.current.style.transition = "none";
+        slideContainer.current.style.transform = `translateX(0px)`;
+
+        //tomamos el primer elemento y lo mandamos al final
+        slideContainer.current.appendChild(firstElement);
+      };
+
+      //Eventlistener
+      slideContainer.current.addEventListener("transitionend", transicion);
+    }
+  }
+
+  useEffect(() => {
+    intervalSlideShow.current = setInterval(() => {
+      next();
+    }, 10000);
+
+    slideContainer.current.addEventListener("mouseenter", () => {
+      clearInterval(intervalSlideShow.current);
+    });
+
+    slideContainer.current.addEventListener("mouseleave", () => {
+      intervalSlideShow.current = setInterval(() => {
+        next();
+      }, 10000);
+    });
+  }, []);
+
   return (
     <div className="slideShow">
-      <div className="slideContainer">
+      <div className="slideContainer" ref={slideContainer}>
         <div className="slide">
-          <img className="img" src={img2} alt="img" />
+          <img className="img" src={img2} alt="img1" />
           <div className="textSlide">
             <p>Descripcion de experiencia</p>
           </div>
         </div>
+
         <div className="slide">
-          <img className="img" src={img1} alt="img" />
+          <img className="img" src={img1} alt="img2" />
           <div className="textSlide">
             <p>Descripcion de experiencia</p>
           </div>
         </div>
-      </div>
-      <div className="controls">
-        <button className="controlLeft">
-          <LeftArrow />
-        </button>
-        <button className="controlRight">
-          <RightArrow />
-        </button>
       </div>
     </div>
   );
 }
-
-export default slideShow;
